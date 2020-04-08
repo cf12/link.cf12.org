357de796a43cbdd3c034df99f18b57531e169c51
CF12 <brianx0011@gmail.com>
2020-04-08 00:14:09 -0400

    Replace lowdb w/ redis

--- src/index.js
+++ src/index.js
@@ -1,30 +1,17 @@
 const express = require('express')
 const rateLimit = require("express-rate-limit")
+const Redis = require('ioredis')
 const helmet = require('helmet')
-
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
-
-const fs = require('fs-extra')
 const path = require('path')
 
-// Database
-const dataDir = path.resolve(__dirname, '..', 'data')
-fs.ensureDirSync(dataDir)
+const stealthKeywords = require('./keywords.json')
 
-const adapter = new FileSync(path.join(dataDir, 'links.json'))
-const db = low(adapter)
-
-db.defaults({ links: {} })
-  .write()
+// Redis
+const client = new Redis(process.env.REDISCLOUD_URL || 'localhost')
 
 // Express + middleware
 const PORT = process.env.PORT || 8080
 const app = express()
-
-const stealthKeywords = [
-  "Discord"
-]
 
 app.use(helmet())
 app.use(express.urlencoded({ extended: false }))
@@ -37,27 +24,25 @@ app.use(rateLimit({
   max: 500
 }))
 
-app.get('/:slug', (req, res) => {
+app.get('/:slug', async (req, res) => {
   const ua = req.header("User-Agent")
-  let stealthRequest = false
+  let stealthRequest = stealthKeywords.some(e => ua.toLowerCase().includes(e))
 
-  for (const keyword of stealthKeywords) {
-    if (ua.includes(keyword)) stealthRequest = true
+  try {
+    const url = await client.hget(req.params.slug, 'url')
+    const stealth = await client.hget(req.params.slug, 'stealth')
+
+    if (url)
+      if (stealthRequest && stealth) res.redirect(stealth)
+      else res.redirect(url)
+    else
+      res.json({ error: 'Invalid link!' })
+  } catch (err) {
+    throw new Error(err)
   }
-
-  const link = db
-    .get('links')
-    .get(req.params.slug)
-    .value()
-
-  if (link)
-    if (stealthRequest) res.redirect(link.stealth)
-    else res.redirect(link.url)
-  else
-    res.json({ error: 'Invalid link!' })
 })
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
   const urlRegex = /^(?:(?:(?:http[s]?):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i
   const slugRegex = /[0-9a-z-]/i
   let { slug, url, stealth } = req.body
@@ -74,18 +59,25 @@ app.post('/', (req, res) => {
     return
   }
 
-  db.get('links')
-    .set(slug, {
-      url,
-      stealth
-    })
-    .write()
+  try {
+    if (await client.exists(slug)) {
+      res.json({
+        error: 'URL already exists!'
+      })
+      return
+    }
 
-  res.json({
-    info: 'URL created successfully!',
-    url: url,
-    slug: slug
-  })
+    client.hset(slug, 'url', url)
+    client.hset(slug, 'stealth', stealth)
+
+    res.json({
+      info: 'URL created successfully!',
+      source: url,
+      slug: slug
+    })
+  } catch (err) {
+    throw new Error(err)
+  }
 })
 
 console.log(`[i] Server listening on port: ${PORT}`)
